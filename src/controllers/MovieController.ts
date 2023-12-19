b7153ff67d357a7098b4fc73ff10f5ce022e1e19
Hebert Soares <hebertsoaresof@gmail.com>
2023-12-19 20:35:30 -0300

    Inserindo dados

--- src/controllers/MovieController.ts
+++ src/controllers/MovieController.ts
@@ -3,5 +3,12 @@ import { MovieModel } from "../models/Movies";
 import Logger from "../../config/logger";
 
 export async function createMovie(req: Request, res: Response){
+  try {
+    const data = req.body
+    const movie = await MovieModel.create(data)
+    return res.status(201).json(movie)
+  } catch (e: any) {
+    Logger.error(`Error no sistema: ${e.message}`)
+  }
   return res.status(200).send("Deu certo")
 }
