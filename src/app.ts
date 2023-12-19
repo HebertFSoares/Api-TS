7225436c3f5c0e7152ee18f604c773f65612894d
Hebert Soares <hebertsoaresof@gmail.com>
2023-12-18 23:32:33 -0300

    Resolvendo conflitos

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,19 @@ import express from "express"
 import config from "config"
 import db from "../config/db"
 import Logger from "../config/logger"
+import router from "./routes"
 
 const app = express()
 
+// Json
 app.use(express.json())
+
+app.use("/api/", router)
 
 // app port
 const port = config.get<number>("port")
 
-app.listen(port, async () =>{
+app.listen(port, async () => {
   await db();
 
   Logger.info(`Aplicação na porta ${port}`)
