f3c23ce4ef5976e91e7172295bd0ff25ebee83f8
Hebert Soares <hebertsoaresof@gmail.com>
2023-12-18 23:40:45 -0300

    Configurando a vaiáveis de ambiente

--- config/default.ts
+++ config/default.ts
@@ -1,5 +1,8 @@
+const dbuser = process.env.DB_USER
+const dbpass = process.env.DB_PASS
+
 export default {
   port: 3000,
-  dbUri: "mongodb+srv://bertadmin:bertadmin@cluster0.q2qp2io.mongodb.net/?retryWrites=true&w=majority",
+  dbUri: `mongodb+srv://${dbuser}:${dbpass}@cluster0.q2qp2io.mongodb.net/?retryWrites=true&w=majority`,
   env: "development"
 };
