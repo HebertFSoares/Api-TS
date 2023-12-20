bf77af9d6ff3487cc0a5792d8db32e9f370320ab
Hebert Soares <hebertsoaresof@gmail.com>
2023-12-20 16:45:39 -0300

    Removendo filmes

--- src/controllers/MovieController.ts
+++ src/controllers/MovieController.ts
@@ -9,8 +9,8 @@ export async function createMovie(req: Request, res: Response){
     return res.status(201).json(movie)
   } catch (e: any) {
     Logger.error(`Error no sistema: ${e.message}`)
+    return res.status(500).json({ error: "Por favor, tente mais tarde!"})
   }
-  return res.status(200).send("Deu certo")
 }
 
 export async function FindMovieById(req:Request, res:Response) {
@@ -24,7 +24,7 @@ export async function FindMovieById(req:Request, res:Response) {
 
     return res.status(200).json()
   } catch (e: any) {
-    Logger.error(`Erro no sistema: ${e.message}`)
+    return res.status(500).json({ error: "Por favor, tente mais tarde!"})
   }
 }
 
@@ -35,5 +35,24 @@ export async function getAllMovies(req: Request, res: Response) {
     return res.status(200).json(movies)
   } catch (e: any) {
     Logger.error(`Erro no sistema: ${e.message}`)
+    return res.status(500).json({ error: "Por favor, tente mais tarde!"})
+  }
+}
+
+export async function removeMovie(req: Request, res:Response) {
+  try {
+    const id = req.params.id
+    const movie = await MovieModel.findById(id)
+
+    if(!movie){
+      return res.status(404).json({error: "O filme não existe"})
+    }
+
+    await movie.deleteOne()
+
+    return res.status(200).json({msg: "Filme removido com sucesso"})
+  } catch (e: any) {
+    Logger.error(`Erro no sistema: ${e.message}`)
+    return res.status(500).json({ error: "Por favor, tente mais tarde!"})
   }
 }
