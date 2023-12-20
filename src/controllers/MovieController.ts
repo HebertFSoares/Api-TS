038e3c754544193b6f87eea40100158e3ad37fd9
Hebert Soares <hebertsoaresof@gmail.com>
2023-12-20 15:27:28 -0300

    Resgatando filme por id

--- src/controllers/MovieController.ts
+++ src/controllers/MovieController.ts
@@ -12,3 +12,18 @@ export async function createMovie(req: Request, res: Response){
   }
   return res.status(200).send("Deu certo")
 }
+
+export async function FindMovieById(req:Request, res:Response) {
+  try {
+    const id = req.params.id
+    const movie = await MovieModel.findById(id)
+
+    if(!movie){
+      return res.status(404).json({ error: "O filme não existe"})
+    }
+
+    return res.status(200).json()
+  } catch (e: any) {
+    Logger.error(`Erro no sistema: ${e.message}`)
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,12 @@
 import { Router, Request, Response } from "express";
-import { createMovie } from "./controllers/MovieController";
+import { FindMovieById, createMovie } from "./controllers/MovieController";
 import { validate } from "./middleware/handleValidation";
 import { movieCreateValidation } from "./middleware/MovieValidation";
 
 const router = Router()
 
 router.get("/ping", (req:Request, res:Response) => res.send('pong')
-).post("/movie", movieCreateValidation, validate, createMovie);
+).post("/movie", movieCreateValidation, validate, createMovie)
+  .get("/movie/:id", FindMovieById);
 
 export default router
