1f7cf07dffec3aa881d79bd753bc41cd6cf6dc9c
Hebert Soares <hebertsoaresof@gmail.com>
2023-12-20 14:36:13 -0300

    Middleware de validação de erros

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,10 @@
 import { Router, Request, Response } from "express";
 import { createMovie } from "./controllers/MovieController";
-
+import { validate } from "./middleware/handleValidation";
 
 const router = Router()
 
 router.get("/ping", (req:Request, res:Response) => res.send('pong')
-).post("/movie", createMovie);
+).post("/movie", validate, createMovie);
 
 export default router
