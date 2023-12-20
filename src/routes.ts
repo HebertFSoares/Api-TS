7b2d01b348ff76dcd631480ead8b7698b47e0828
Hebert Soares <hebertsoaresof@gmail.com>
2023-12-20 14:56:33 -0300

    Validação dos filmes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import { Router, Request, Response } from "express";
 import { createMovie } from "./controllers/MovieController";
 import { validate } from "./middleware/handleValidation";
+import { movieCreateValidation } from "./middleware/MovieValidation";
 
 const router = Router()
 
 router.get("/ping", (req:Request, res:Response) => res.send('pong')
-).post("/movie", validate, createMovie);
+).post("/movie", movieCreateValidation, validate, createMovie);
 
 export default router
